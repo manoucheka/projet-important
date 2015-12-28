a080c6212497f37bb925cd9b43ad9d5a0ea82c2e
= <=>
2015-12-28 02:45:37 -0800

    ddd

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -18,6 +18,7 @@ Route::get('/', function () {
 Route::resource('flyers', 'FlyersController');
 
 
+Route::resource('reports', 'ReportsController');
 
 /*
 |--------------------------------------------------------------------------
